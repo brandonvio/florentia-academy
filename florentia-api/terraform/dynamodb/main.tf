696179237dd0c780ad3952211e20e2841035317c
Brandon Vicedomini <brandonvio@outlook.com>
2023-08-11 14:56:06 -0700

    #9 seeding the data

--- florentia-api/terraform/dynamodb/main.tf
+++ florentia-api/terraform/dynamodb/main.tf
@@ -1,12 +1,10 @@
 # Terraform module for creating a DynamoDB table for the Course Enrollment service.
 
 resource "aws_dynamodb_table" "florentia_academy_db" {
-  name           = "florentia_academy_db"
-  billing_mode   = "PROVISIONED"
-  read_capacity  = 15
-  write_capacity = 15
-  hash_key       = "pk"
-  range_key      = "sk"
+  name         = "florentia_academy_db"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "pk"
+  range_key    = "sk"
 
   attribute {
     name = "pk"
